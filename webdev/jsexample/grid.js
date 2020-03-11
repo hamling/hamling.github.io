90a5f12ab816085eacbadd81c000b61b0df8f724
Greg Hamlin <gjhamlin@gmail.com>
2020-03-11 07:12:39 -0400

    gridddd

--- webdev/jsexample/grid.js
+++ webdev/jsexample/grid.js
@@ -8,6 +8,15 @@ button.onclick = function () {
     var numRows = document.getElementById("rows").value;
     var numCols = document.getElementById("cols").value;
 
+    if (numRows < 1 || numRows > 100) {
+        alert("No!");
+        return;
+    }
+
+    if (numCols < 1 || numCols > 100) {
+        alert("No!!");
+        return;
+    }
     //console.log(numRows);
     //console.log(numCols);
 
@@ -27,8 +36,27 @@ button.onclick = function () {
     console.log(rowval);
     console.log(rowval.repeat(numRows));
 
+    while (board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
 
-//    board.style.setProperty("grid-template-rows", "???");
+    let tableBody = document.createElement("tbody");
+
+    board.appendChild(tableBody);
+
+    for (let row = 0; row < numRows; row++) {
+        let tableRow = document.createElement("tr");
+        tableBody.appendChild(tableRow);
+
+        for (let col = 0; col < numCols; col++) {
+            let tableData = document.createElement("td");
+            tableRow.appendChild(tableData);
+            tableData.appendChild(document.createTextNode("X"));
+        }
+    }
+
+    board.style.setProperty("grid-template-columns", "repeat(" + numCols + ",1fr)");
+    board.style.setProperty("grid-template-rows", "repeat(" + numRows + ",1fr)");
 
     // make sure the table gets that number of rows (tr) and columns (td)
 
